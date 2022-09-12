5d01b219a382054152d7b0dfd616c9f41faefd38
an_bir <an_bir@mail.com>
2022-09-12 20:56:40 +0300

    add server routes

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process');
 const express = require('express');
 const path = require('path');
 const PORT = 3000;
@@ -6,10 +7,48 @@ const app = express();
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(
     path.join(__dirname + '/../../dist/login.html')
   );
 })
 
-app.listen(PORT);
+app.get('/signin', (req, res) => {
+  res.sendFile(
+    path.join(__dirname + '/../../dist/signin.html')
+  );
+})
+
+app.get('/main', (req, res) => {
+  res.sendFile(
+    path.join(__dirname + '/../../dist/main.html')
+  );
+})
+
+app.get('/profile', (req, res) => {
+  res.sendFile(
+    path.join(__dirname + '/../../dist/profile.html')
+  );
+})
+
+app.get('/505', (req, res) => {
+  res.sendFile(
+    path.join(__dirname + '/../../dist/505.html')
+  );
+})
+
+app.get('/', (req, res) => {
+  res.redirect('/login')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname + '/../../dist/404.html')
+  );
+})
+
+
+
+app.listen(PORT, () => {
+  exec(`start http://localhost:${PORT}`);
+});
