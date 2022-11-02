c2593421be9065e4f3aab39f0800ee7366d4e5a1
an_bir <an_bir@mail.com>
2022-11-02 19:55:38 +0300

    modified

--- src/core/base/childrenController/ChildrenController.ts
+++ src/core/base/childrenController/ChildrenController.ts
@@ -3,13 +3,13 @@ import { Component } from '../component';
 import { ISimpleObject, TPreComponent } from '../models';
 
 export class ChildrenController {
-  private _initialChildren: TPreComponent[] = [];
+  private _initialChildren: Record<string, TPreComponent> = {};
   private _children: Record<string, Component | Component[]> = {};
   private _childProps: Record<string, ISimpleObject | ISimpleObject[]> = {};
   private _parent: Element | null = null;
 
   constructor(children: TPreComponent[]) {
-    this._initialChildren = children;
+    this._initialChildren = this._parseChildArray(children);
     this._registerChildren(children, this._setProps.bind(this));
   }
 
@@ -25,22 +25,6 @@ export class ChildrenController {
     }
   }
 
-  private _setChildren(child: Component, id: string, key?: number) {
-    const record = this._children[id];
-    if (key === undefined) {
-      this._children[id] = child;
-    } else {
-      let childArray: Component[] = [];
-      if (Array.isArray(record)) childArray = record;
-      this._children[id] = [...childArray.slice(0, key), child, ...childArray.slice(key + 1)];
-    }
-  }
-
-  private _getChildParentArray(id: string) {
-    if (!this._parent) return;
-    return this._parent.querySelectorAll(`[data-child="${id}"]`);
-  }
-
   private _registerChildren(children: TPreComponent[], callback: (id: string, hash: ISimpleObject) => void) {
     children.forEach((child) => {
       Handlebars.registerHelper(child.prototype.name, ({ hash }: HelperOptions) => {
@@ -50,63 +34,72 @@ export class ChildrenController {
     });
   }
 
+  private _getChildParentArray(id: string) {
+    if (!this._parent) return;
+    return this._parent.querySelectorAll(`[data-child="${id}"]`);
+  }
+
+  private _parseChildArray(children: TPreComponent[]) {
+    return children.reduce<Record<string, TPreComponent>>((acc, child) => {
+      acc[child.prototype.id] = child;
+      return acc;
+    }, {});
+  }
+
+  private _updateChild(id: string, parent: Element, props: ISimpleObject, child?: Component) {
+    if (child) {
+      child.setParentElement(parent);
+      child.setProps(props);
+      return child;
+    }
+    const newChild = this._initialChildren[id](props);
+    newChild.setParentElement(parent);
+    newChild.mount();
+    return newChild;
+  }
+
+  private _updateChildArray(
+    id: string,
+    parents: NodeListOf<Element>,
+    propsArray: ISimpleObject[],
+    childArray: Component[],
+  ) {
+    return propsArray.reduce<Component[]>((acc, prop, idx) => {
+      const child = childArray[idx];
+      acc[idx] = this._updateChild(id, parents[idx], prop, child);
+      return acc;
+    }, []);
+  }
+
   public mountChildren() {
-    Object.keys(this._childProps).forEach((key) => {
+    this._children = Object.keys(this._childProps).reduce<Record<string, Component | Component[]>>((acc, key) => {
       const parents = this._getChildParentArray(key);
-      if (!parents) return;
-      const initChild = this._initialChildren.find((item) => item.prototype.id === key);
-      if (!initChild) return;
-      const props = this._childProps[key];
-      if (Array.isArray(props)) {
-        props.forEach((props, idx) => {
-          const child = initChild(props);
-          child.setParentElement(parents[idx]);
-          child.mount();
-          this._setChildren(child, key, idx);
-        });
+      const propsRecord = this._childProps[key];
+      if (!parents) return acc;
+      if (Array.isArray(propsRecord)) {
+        acc[key] = this._updateChildArray(key, parents, propsRecord, []);
       } else {
-        const child = initChild(props);
-        child.setParentElement(parents[0]);
-        child.mount();
-        this._setChildren(child, key);
+        acc[key] = this._updateChild(key, parents[0], propsRecord);
       }
-    });
+      return acc;
+    }, {});
   }
 
   public updateChildren() {
-    Object.keys(this._childProps).forEach((key) => {
+    this._children = Object.keys(this._childProps).reduce<Record<string, Component | Component[]>>((acc, key) => {
       const propsRecord = this._childProps[key];
       const childRecord = this._children[key];
       const parents = this._getChildParentArray(key);
-      if (!parents) return;
-      const initChild = this._initialChildren.find((item) => item.prototype.id === key);
-      if (Array.isArray(propsRecord) && Array.isArray(childRecord)) {
-        propsRecord.forEach((props, idx) => {
-          const child = childRecord[idx];
-          if (child) {
-            child.setParentElement(parents[idx]);
-            child.setProps(props);
-          } else {
-            if (!initChild) return;
-            const newChild = initChild(props);
-            newChild.setParentElement(parents[idx]);
-            newChild.mount();
-            this._setChildren(newChild, key, idx);
-          }
-        });
-      } else {
-        if (childRecord instanceof Component) {
-          childRecord.setParentElement(parents[0]);
-          childRecord.setProps(propsRecord);
-        } else {
-          if (!initChild) return;
-          const newChild = initChild(propsRecord);
-          newChild.setParentElement(parents[0]);
-          newChild.mount();
-          this._setChildren(newChild, key);
+      if (parents) {
+        if (Array.isArray(propsRecord) && Array.isArray(childRecord)) {
+          acc[key] = this._updateChildArray(key, parents, propsRecord, childRecord);
+        }
+        if (!Array.isArray(childRecord)) {
+          acc[key] = this._updateChild(key, parents[0], propsRecord, childRecord);
         }
       }
-    });
+      return acc;
+    }, {});
   }
 
   public setParent(elem: Element) {
