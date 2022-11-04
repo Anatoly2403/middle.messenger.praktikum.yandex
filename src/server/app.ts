e33c72c8bfa334f9deef66f1c2ba13a0edd47f32
an_bir <an_bir@mail.com>
2022-11-04 13:15:31 +0300

    modified

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,33 +1,10 @@
-import express from 'express';
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('dist'));
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/login.html'));
-});
-
-app.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/signin.html'));
-});
-
-app.get('/main', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/main.html'));
-});
-
-app.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/profile.html'));
-});
-
-app.get('/500', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/500.html'));
-});
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/404.html'));
-});
+app.use(express.static(path.join(__dirname + '/../../dist')));
 
 app.listen(PORT);
